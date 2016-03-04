9490b8fe567efe0d66cef9d43748f55064f4e099
Mathieu Gagnon <mathieu@gagnon.name>
2016-03-04 13:13:23 -0500

    Simplify tests

--- IniMLTest.php
+++ IniMLTest.php
@@ -2,74 +2,130 @@
 
 class IniMLTest extends \PHPUnit_Framework_TestCase
 {
-    public static $tests = [
-
-        "key: value",
-        [ 'key' => 'value' ],
-
-        "key  :value",
-        [ 'key' => 'value' ],
-
-        "age: 42\nlikes_ice_cream: false",
-        [ 'age' => 42, 'likes_ice_cream' => false ],
-
-        "key: null",
-        [ 'key' => null ],
-
-        "name: frank
-         age: 52
-         name: vincent
-         age: 64",
-        [ [ 'name' => 'frank', 'age' => 52 ],
-          [ 'name' => 'vincent', 'age' => 64 ] ],
-
-        "\\key: value",
-        [ 'key: value' ],
-
-        "key: value\nJanes is this working ?",
-        [ 'key' => 'value', 'Janes is this working ?' ],
-
-        "milk\ncereals",
-        [ 'milk', 'cereals' ],
-
-        "[groceries]\nmilk\ncereals",
-        [ 'groceries' => [ 'milk', 'cereals' ] ],
-
-        "[groceries]\nname:milk",
-        [ 'groceries' => [ [ 'name' => 'milk' ] ] ],
-
-        "[grocery]\nname:milk",
-        [ 'grocery' => [ 'name' => 'milk' ] ],
-
-        "title: Funky\ncontent:\n\nSomething along\nthe lines",
-        [ 'title' => 'Funky', 'content' => "\nSomething along\nthe lines" ],
-
-    ];
-
     public function setUp()
     {
         $this->iniML = new IniML();
     }
 
-    public function testAll()
+    public function testBasicKeyValue()
     {
-        for ($i = 0; $i < count(self::$tests); $i++) {
-            $input = self::$tests[$i++];
-            $output = self::$tests[$i];
-            $this->assertSame($output, $this->iniML->parse($input));
-        }
+        $this->assertEquals(['key' => 'value'],
+                            $this->iniML->parse("key: value"));
     }
 
-    public function testEmit()
+    public function testDelimiterOption()
+    {
+        $this->iniML->options['delimiter'] = '=';
+        $this->assertEquals(
+            [ 'key' => 'value' ],
+            $this->iniML->parse('key = value')
+        );
+    }
+
+    public function testDuplicateKeys()
+    {
+        $this->assertEquals(
+            [ [ 'name' => 'frank', 'age' => 52 ],
+              [ 'name' => 'vincent', 'age' => 64 ] ],
+            $this->iniML->parse('name: frank
+age: 52
+name: vincent
+age: 64')
+        );
+    }
+
+    public function testLine()
+    {
+        $this->assertEquals(
+            [ 'key' => 'value', 'Jane is this working ?' ],
+            $this->iniML->parse('key: value
+Jane is this working ?
+')
+        );
+    }
+
+    public function testLineEscape()
+    {
+        $this->assertEquals(
+            [ 'key: value' ],
+            $this->iniML->parse('\\key: value')
+        );
+    }
+
+    public function testMultipleLines()
+    {
+        $this->assertEquals(
+            [ 'milk', 'cereals' ],
+            $this->iniML->parse('milk
+cereals')
+        );
+    }
+
+    public function testSections()
+    {
+        $this->assertEquals(
+            [ 'groceries' => [ 'milk', 'cereals' ] ],
+            $this->iniML->parse('[groceries]
+milk
+cereals')
+        );
+    }
+
+    public function testSectionPlural()
+    {
+        $this->assertEquals(
+            [ 'groceries' => [ [ 'name' => 'milk' ] ] ],
+            $this->iniML->parse('[groceries]
+name: milk')
+        );
+    }
+
+    public function testSectionSingular()
+    {
+        $this->assertEquals(
+            [ 'grocery' => [ 'name' => 'milk' ] ],
+            $this->iniML->parse('[grocery]
+name: milk')
+        );
+    }
+
+    public function testMultiline()
+    {
+        $this->assertEquals(
+            [ 'title' => 'Funky',
+              'content' => 'Something along
+the lines'
+            ],
+            $this->iniML->parse('title: Funky
+content:
+Something along
+the lines')
+        );
+    }
+
+    public function testEmitSection()
     {
         $input = "[groceries]\nmilk\napples\nkey: value\n";
-        $this->assertSame($input, $this->iniML->emit($this->iniML->parse($input), ': '));
+        $this->assertSame($input,
+                          $this->iniML->emit($this->iniML->parse($input)));
+    }
 
+    public function testEmitEscape()
+    {
         $input = "\\key: value\n";
-        $this->assertSame($input, $this->iniML->emit($this->iniML->parse($input)));
+        $this->assertSame($input,
+                          $this->iniML->emit($this->iniML->parse($input)));
+    }
 
-        $this->assertSame("enable: false\n", $this->iniML->emit([ 'enable' => false ]));
+    public function testEmitBoolean()
+    {
+        $this->assertSame("enable: false\n",
+                          $this->iniML->emit([ 'enable' => false ]));
+    }
 
-        $this->assertSame("enable: null\n", $this->iniML->emit([ 'enable' => null ]));
+    public function testEmitNull()
+    {
+        $this->assertSame("enable: null\n",
+                          $this->iniML->emit([ 'enable' => null ]));
     }
 }
